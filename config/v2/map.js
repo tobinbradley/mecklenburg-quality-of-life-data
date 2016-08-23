61aa3e1347f4c4d2a802cec4fdbea09a11af5ce7
tobinbradley <tobin.bradley@gmail.com>
2016-08-23 13:46:23 -0400

    map config settings

--- config/v2/map.js
+++ config/v2/map.js
@@ -1,1 +1,10 @@
- 
+let mapConfig = {
+    style: "./style/style.json",
+    zoom: 9.5,
+    center: [-80.815,35.31],
+    maxBounds: [[-78.255, 37.384],[-83.285, 33.180]],
+    minZoom: 8,
+    preserveDrawingBuffer: navigator.userAgent.toLowerCase().indexOf('firefox') > -1  // fix for Firefox print
+};
+
+module.exports = mapConfig;
