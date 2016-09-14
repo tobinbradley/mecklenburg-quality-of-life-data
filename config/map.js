2577f376fc47ec640496c8ee055413c7053b0ad2
tobinbradley <tobin.bradley@gmail.com>
2016-09-14 18:55:42 -0400

    data fix and configuration changes

--- config/map.js
+++ config/map.js
@@ -1,8 +1,10 @@
 let mapConfig = {
     style: "./style/style.json",
-    zoom: 9.5,
-    center: [-80.815,35.31],
-    maxBounds: [[-78.255, 37.384],[-83.285, 33.180]],
+    zoomEmbed: 9.5,
+    zoom: 9.3,
+    centerEmbed: [-80.815,35.31],
+    center: [-80.84, 35.26],
+    maxBounds: [[-83.285, 33.180],[-78.255, 37.384]],
     minZoom: 8,
     preserveDrawingBuffer: navigator.userAgent.toLowerCase().indexOf('firefox') > -1  // fix for Firefox print
 };
