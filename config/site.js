a05edf152b24508f0d62692175ed033fb68285fd
tobinbradley <tobin.bradley@gmail.com>
2016-09-19 16:14:48 -0400

    more config changes

--- config/site.js
+++ config/site.js
@@ -1,49 +1,31 @@
 let siteConfig = {
     "neighborhoodDescriptor": "NPA",
     "gaKey": "UA-48797957-1",
-    "header": {
-        "description": "Quality of Life Dashboard.",
-        "keywords": "GIS,Mecklenburg County,parks,libraries,schools,environment,impervious surface,floodplain,zoning,assessment,tax,photos,government",
-        "author": "Tobin Bradley",
-        "title": "Quality of Life Explorer"
-    },
+    "keywords": "GIS,Mecklenburg County,parks,libraries,schools,environment,impervious surface,floodplain,zoning,assessment,tax,photos,government",
+    "author": "Tobin Bradley",
+    "title": "Quality of Life Explorer",
     "description": "Exploring neighborhood character, economics, education, engagement, environment, health, housing, safety and transportation.",
     "qolembedURL": "http://mcmap.org/qol-embed/",
     "qolreportURL": "http://mcmap.org/qol-report/",
     "qoldashboardURL": "http://mcmap.org/qol/",
-    "menu": {
-        "About": {
-            "links": [
-                {
-                    "href": "downloads/dimensiononepage.pdf",
-                    "text": "What is in the Dashboard"
-                },
-                {
-                    "href": "http://qol.charmeck.org/",
-                    "text": "About the Project"
-                }
-            ]
+    "links": [
+        {
+            "href": "http://qol.charmeck.org/",
+            "text": "About"
         },
-        "Get Data": {
-            "links": [
-                {
-                    "href": "downloads/qol-data.zip",
-                    "text": "Download the Quality of Life Dataset"
-                },
-                {
-                    "divider": true
-                },
-                {
-                    "href": "http://maps.co.mecklenburg.nc.us/openmapping/",
-                    "text": "Mecklenburg County Open Mapping"
-                },
-                {
-                    "href": "http://clt.charlotte.opendata.arcgis.com/",
-                    "text": "City of Charlotte Open Data Portal"
-                }
-            ]
+        {
+            "href": "downloads/qol-data.zip",
+            "text": "Download Data"
+        },
+        {
+            "href": "http://maps.co.mecklenburg.nc.us/openmapping/",
+            "text": "Mecklenburg County Open Mapping"
+        },
+        {
+            "href": "http://clt.charlotte.opendata.arcgis.com/",
+            "text": "City of Charlotte Open Data Portal"
         }
-    }
+    ]
 };
 
 module.exports = siteConfig;
