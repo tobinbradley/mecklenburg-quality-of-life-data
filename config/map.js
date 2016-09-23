d380adbf3855ea7d6c555dd072e225d8be75580e
tobinbradley <tobin.bradley@gmail.com>
2016-09-23 17:06:37 -0400

    added osm-liberty style

--- config/map.js
+++ config/map.js
@@ -8,12 +8,16 @@
 //                ||     ||
 //
 //
-// style        gl style json, could be your tiles or mapbox or whatever
+// style        gl style json, could be your tiles or mapbox or whatever. I'd recommend
+//              using the great osm-liberty style from https://github.com/lukasmartinelli/osm-liberty,
+//              which is included. Just change style to "./style/osm-liberty.json"
+//
+//
 // zoomEmbed    default zoom level for embed map
 // zoom         default zoom level for dashboard map
 // centerEmbed  center coordinates for embed map
 // center       center coordinates for dashboard map
-// maxBounds    max bounds so your users don't go zipping around the planet
+// maxBounds    max bounds so your users don't go zipping around the planet. format LL, UR
 // minZoom      minimum zoom level
 // preserveDrawingBuffer    this is to make sure Firefox can print the map
 // neighborhoodsSelectedBefore      the layer id in your map style to drop the selected line before, usually the first labels
