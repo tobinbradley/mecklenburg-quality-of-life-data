683fdeac02cc76a9822373d65e23b00a2e2435c9
tobinbradley <tobin.bradley@gmail.com>
2016-09-27 07:09:20 -0400

    jshint yelled at me

--- test/csv-file-format.js
+++ test/csv-file-format.js
@@ -9,8 +9,8 @@ var geoJSON = require("../geography.geojson.json");
 // natural sort for mixed character and string
 function naturalCompare(a, b) {
     var ax = [], bx = [];
-    a.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { ax.push([$1 || Infinity, $2 || ""]) });
-    b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]) });
+    a.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { ax.push([$1 || Infinity, $2 || ""]); });
+    b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]); });
     while(ax.length && bx.length) {
         var an = ax.shift();
         var bn = bx.shift();
